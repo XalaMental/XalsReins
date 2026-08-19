21b7bf643ee1e3b5ca87b720594049ca49511d32
XalaMental <s79j2jb2c2@privaterelay.appleid.com>
2026-08-19 00:09:15 -0400

    - Sets up the addon's internal pieces so every part can find the others.

---
+++ Bootstrap.lua
@@ -0,0 +1,15 @@
+-- Bootstrap.lua
+-- Xal's Reins
+local addonName, addonTable = ...
+
+-- Main namespace definition - add a line here for every module file as it's built
+addonTable.BrandStyle    = {}
+addonTable.MountData     = {}
+addonTable.WhatsNew      = {}
+addonTable.Engine        = {}
+addonTable.MainTracker   = {}
+addonTable.MountDetail   = {}
+addonTable.CollectionWindow = {}
+addonTable.SettingsPanel = {}
+addonTable.MinimapButton = {}
+addonTable.ChatCommands  = {}
